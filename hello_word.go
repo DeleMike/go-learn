427697d88362c78810553209e4917286534838cc
Akindele Michael <akindelemichael65@gmail.com>
2024-09-30 11:31:32 +0100

    concluded variables

--- hello_word.go
+++ hello_word.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	accountAge := 2.6
-	accountAgeInt := int64(accountAge)
+	// accountAge := 2.6
+	// accountAgeInt := int(accountAge)
 
-	fmt.Println("Your account has existed for", accountAgeInt, "years")
+	// fmt.Println("Your account has existed for", accountAgeInt, "years")
+	// fmt.Printf("I am %v years old", 10)
+
+	playWithFormatting()
+}
+
+func playWithFormatting() {
+	const name = "Andrew ng"
+	const openRate = 30.5
+
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent", name, openRate)
+
+	fmt.Printf(msg)
 }
