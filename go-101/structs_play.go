170b2ce7dfc5441bd1d2b678090ff4461a4e906d
Akindele Michael <akindelemichael65@gmail.com>
2024-12-14 17:59:54 +0100

    using pointers to update data

--- go-101/structs_play.go
+++ go-101/structs_play.go
@@ -2,15 +2,44 @@ package main
 
 import "fmt"
 
-func main() {
-	var myStruct struct {
-		number float64
-		word   string
-		toggle bool
-	}
+type subscriber struct {
+	name   string
+	rate   float64
+	active bool
+}
 
-	myStruct.number = 3.14
-	myStruct.word = "hello"
-	//myStruct.toggle = true
-	fmt.Println(myStruct.number, "||", myStruct.word, "||", myStruct.toggle)
+func applyDiscount(s *subscriber) {
+	s.rate = 4.99
+}
+
+func main() {
+	//myStruct.number = 3.14
+	//myStruct.word = "hello"
+	////myStruct.toggle = true
+	//fmt.Println(myStruct.number, "||", myStruct.word, "||", myStruct.toggle)
+	//var bolts part
+	//bolts.description = "This is a 6 by 6 bolt"
+	//bolts.count = 18
+	//showInfo(bolts)
+
+	//var s subscriber
+	//applyDiscount(&s)
+	//fmt.Println(s.rate)
+
+	var value myStruct
+	value.myField = 3
+	var pointer *myStruct = &value
+	fmt.Println((*pointer).myField)
+}
+
+type myStruct struct {
+	myField int
+}
+type part struct {
+	description string
+	count       int
+}
+
+func showInfo(p part) {
+	fmt.Println(p.description, "||", p.count)
 }
