f155c102e4f9ea68756284b3ed2a3f35c8439c3a
Akindele Michael <akindelemichael65@gmail.com>
2024-12-30 15:31:58 +0100

    update recipe by PUT and PATCH

--- recipe_api/main.go
+++ recipe_api/main.go
@@ -16,6 +16,8 @@ func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipe/:id", UpdateRecipeHandler)
+	router.PATCH("/recipe/:id", UpdateRecipeByPatchHandler)
 	err := router.Run(":8080")
 	if err != nil {
 		return
@@ -53,4 +55,72 @@ func NewRecipeHandler(c *gin.Context) {
 
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
+func UpdateRecipeByPatchHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+	foundRecipe := recipes[index]
+	updateRecipe(recipe, &foundRecipe)
+	recipes[index] = foundRecipe
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe updated successfully",
+		"recipe":  foundRecipe,
+	})
+}
+
+func updateRecipe(recipe Recipe, foundRecipe *Recipe) {
+	if recipe.Name != "" {
+		foundRecipe.Name = recipe.Name
+	}
+	if recipe.Tags != nil {
+		foundRecipe.Tags = recipe.Tags
+	}
+	if recipe.Ingredients != nil {
+		foundRecipe.Ingredients = recipe.Ingredients
+	}
+	if recipe.Instructions != nil {
+		foundRecipe.Instructions = recipe.Instructions
+	}
+	if recipe.PublishedAt != "" {
+		foundRecipe.PublishedAt = time.Now().Format(time.RFC3339)
+	}
 }
